f735abcec2f8976828b06b6372aefbb071ad224d
Kevin Grandon <kevingrandon@yahoo.com>
2019-01-04 14:57:36 -0800

    Ignore graphql module due to flow@0.89 errors
    https://github.com/uber-workflow/fusion-release/pull/190

---
+++ flow-typed/npm/graphql.js
@@ -0,0 +1,6 @@
+// @flow
+// Temporarily ignore graphql js lib until fixes in graphql@14.0.3 is released
+// Corresponding reference in .flowconfig should be removed along with this.
+declare module 'graphql' {
+  declare module.exports: any;
+}
